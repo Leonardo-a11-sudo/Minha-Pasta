c150955f56351a9229d7cfc46b28952ca6232a23
Leonardo-a11-sudo <57409466+Leonardo-a11-sudo@users.noreply.github.com>
2019-11-05 21:51:48 -0400

    Add files via upload

---
+++ DDOS (EXTRAÇÃO DE DADOS.bat
@@ -0,0 +1,27 @@
+
+
+@echo off
+title DDOS
+cls
+
+color 0a
+msg * INICIANDO BUSCAS INTERNAS...
+pause
+msg *   CONEXOES ATIVAS
+netstat -n
+pause
+msg *  DADOS DE COFIGURACAO DE IP DO WINDOWS
+ipconfig
+pause
+msg * VELOCIDADE DE CONEXAO
+ping 192.168.0.105
+pause
+
+
+msg * DADOS EXTRAIDOS COM SUCESSO
+
+msg * RESULTADOS OBTIDOS COM EXITO
+msg * PRESSIONE ENTER PARA SAIR
+pause
+
+
